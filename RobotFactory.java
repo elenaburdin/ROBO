091c18fa4ad492df8ab6e7d3f3db6e0f8c6300ca
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2020-07-29 21:03:38 +0300

    Add files via upload

---
+++ RobotFactory.java
@@ -0,0 +1,10 @@
+package robot;
+
+public class RobotFactory {
+
+	public static AbstractRobot getRobot(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
